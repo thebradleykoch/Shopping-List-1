e5b454ec42a636bf23be4fc3bea5848c2a213924
Jrej <33304306+Jrejoire@users.noreply.github.com>
2019-02-17 05:31:48 +0100

    Add files via upload
    Adding "Delete" feature - Clickable bin icon to delete items from the list.

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.getElementById("list");
+var binlist = document.getElementById("bin");
+
 
 
 function inputLength() {
@@ -13,23 +15,46 @@ function createListElement() {
 	ul.appendChild(li);
 	input.value = "";
 }
+function createBinElement(){
+	var binimg = document.createElement("IMG");
+	binimg.setAttribute("class","binicon");
+	binimg.setAttribute("src","https://cdn3.iconfinder.com/data/icons/cleaning-icons/512/Trash_Can-512.png");
+	binimg.setAttribute("alt","bin");
+	binimg.setAttribute("height","20px");
+	binimg.setAttribute("width", "20px");
+	var libin =document.createElement("li");
+	libin.setAttribute("class",ul.lastElementChild.innerText);
+	libin.appendChild(binimg);
+	binlist.appendChild(libin);
+}
 
 function addListAfterClick() {
 	if (inputLength() > 0) {
 		createListElement();
-		}
+		createBinElement();
+	}
 }
 
 function addListAfterKeypress(event) {
 	if (inputLength() > 0 && event.keyCode === 13) {
 		createListElement();
-		}
+		createBinElement();
+	}
 }
 
 function toggleDoneClass () {
 	this.classList.toggle("done");
 }
 
+function removeListAfterClickBin (){
+	this.parentNode.removeChild(this);
+	var linkedclass = this.className;
+	var items = ul.children;
+	for(var i=0;i<items.length;i++)
+	if (items[i].innerText === linkedclass){
+	items[i].parentNode.removeChild(items[i]);
+	}
+}
 
 function clickList (){
 	var items = ul.children;
@@ -37,6 +62,11 @@ function clickList (){
 	items[i].addEventListener("click", toggleDoneClass);}
 }
 
+function clickBin (){
+	bins = binlist.children;
+	for (var i=0; i<bins.length; ++i){
+	bins[i].addEventListener("click", removeListAfterClickBin);}
+}
 
 button.addEventListener("click", addListAfterClick);
 
@@ -44,3 +74,4 @@ input.addEventListener("keypress", addListAfterKeypress);
 
 ul.addEventListener("click", clickList);
 
+binlist.addEventListener("click", clickBin);
